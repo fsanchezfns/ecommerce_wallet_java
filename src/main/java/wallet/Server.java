fb99c3fd8805afb74e1d66a2527956a72fa80612
Franco <fsanchez.fns@gmail.com>
2020-11-19 23:57:20 -0300

    Prueba de controller + cambio de IDE

--- src/main/java/wallet/Server.java
+++ src/main/java/wallet/Server.java
@@ -1,9 +1,33 @@
 package wallet;
 
+import wallet.application.RestController;
 import static spark.Spark.*;
 
 public class Server{
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hellow word");
+        
+        new Server().start();
+    }
+
+    void start(){
+
+        path("/v1", () -> {
+
+            path("/operations", () -> {
+                post("",  (req, res) -> RestController.createOperation(req,res));
+                get("/:id" ,  (req, res) -> RestController.getOperation(req,res) );
+                get("",  (req, res) -> "consulta todas las operaciones");  
+                
+            });
+
+            path("/wallet", () -> {
+                get("",  (req, res) -> "consulta la billetera");
+                
+            });
+
+
+
+        });
+
     }
 }
---
+++ src/main/java/wallet/application/RestController.java
@@ -0,0 +1,22 @@
+package wallet.application;
+
+
+import spark.Request;
+import spark.Response;
+
+public class RestController{
+
+    public static String createOperation(Request req, Response res) {
+
+        
+        return "asdasdcreateOperationasd";
+        
+    }
+
+    public static String getOperation(Request req, Response res) {
+        
+        return "getOperation";
+        
+    }
+    
+}
